707666bb9f89234f83e91ab440839362b76430c2
Dmitriy Chudnyi <dmitriy@chudnyi.com>
2024-03-19 23:01:03 +0300

    fix: fix shebang prefix and deno shell command

--- src/_config/const.ts
+++ src/_config/const.ts
@@ -7,10 +7,10 @@ export const SchemaURL = `https://clispec.org/schemas/clispec.json`;
 export const CliDescription =
   "The interpreter of the open CLI specification: https://clispec.org";
 export const ExecuteFileMode = 0o700;
-export const RunShebangPrefix = "#!/usr/bin/env";
+export const RunShebangPrefix = "#!/usr/bin/env -S";
 export const DefaultRunShell: shell = "bash";
 export const RunShellCommands: { [shell: string]: string } = {
-  deno: "deno run -A",
+  deno: "deno run -q -A",
 };
 export const RunShellHeaders: { [shell: string]: string[] } = {
   bash: ["set -eo pipefail"],
